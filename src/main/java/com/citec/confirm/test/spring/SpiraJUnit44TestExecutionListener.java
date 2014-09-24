c8cb9433bad41b1bc90ba6e999811c9f2c2023a1
William Dutton <william.dutton@citec.com.au>
2014-09-24 05:04:39 +0000

    update spirateam junit extension to use latest commons-lang3 and have provided spiratest framework
    git-svn-id: file:///home/duttonw/svnDump/confirm/test/tools/spirateamjunitextension/trunk@35690 99543e3d-47ae-4703-d4a0-f5f94a2d398a

--- src/main/java/com/citec/confirm/test/spring/SpiraJUnit44TestExecutionListener.java
+++ src/main/java/com/citec/confirm/test/spring/SpiraJUnit44TestExecutionListener.java
@@ -2,8 +2,8 @@ package com.citec.confirm.test.spring;
 
 
 import com.inflectra.spiratest.addons.junitextension.*;
-import org.apache.commons.lang.StringUtils;
-import org.apache.commons.lang.Validate;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.Validate;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.test.context.TestContext;
